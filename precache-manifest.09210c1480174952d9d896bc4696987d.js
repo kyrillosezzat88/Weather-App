7a96656c7be029d8272ef23c9bd00190e695b4c1
kyrillosezzat <kyrillosezzat300@gmail.com>
2019-08-27 03:39:02 -0700

    Updates

---
+++ precache-manifest.09210c1480174952d9d896bc4696987d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/Weather-App/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "89bb695666ed5c38f231",
+    "url": "/Weather-App/static/js/runtime~main.671df5a1.js"
+  },
+  {
+    "revision": "62af525d8458d7a9ee59",
+    "url": "/Weather-App/static/js/main.2adb4e92.chunk.js"
+  },
+  {
+    "revision": "f8dd3689f97c92f07b30",
+    "url": "/Weather-App/static/js/2.8776f431.chunk.js"
+  },
+  {
+    "revision": "62af525d8458d7a9ee59",
+    "url": "/Weather-App/static/css/main.e1b42e76.chunk.css"
+  },
+  {
+    "revision": "438cbe5cbb15451d930682c0e2a922f5",
+    "url": "/Weather-App/index.html"
+  }
+];
